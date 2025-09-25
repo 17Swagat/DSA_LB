b01ade1ebe4096ee346b364488eee382a43af513
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 20:09:08 +0530

    STL -> PriorityQueue 0.3: MinHeap creation. Elements with smallest value gets the highest priority

--- DSA_RED/STL/Containers/priorityQueue_.cpp
+++ DSA_RED/STL/Containers/priorityQueue_.cpp
@@ -1,23 +1,12 @@
-#include <iostream> 
+#include <iostream>
 #include <queue>
 
 using namespace std;
 
-
-int main(){
-    priority_queue<int> pq; // max heap :=> "Largest element at the top (i.e Gets Highest Priority)"
-
-    pq.push(10);
-    pq.push(20);
-    pq.push(30);
-    pq.push(22);
-
-    //*  iterating through the priority queue is not possible
-    while (!pq.empty()) {
-        cout << pq.top() << " "; // 30 22 20 10
-        pq.pop();
-    }
-
+int main()
+{
+    // min-heap
+    priority_queue<int,vector<int>,greater<int>>pq; // min-heap
 
     return 0;
 }
