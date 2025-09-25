8db182b4b6e5db4e07809910f62c21ea688bb4db
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 17:52:15 +0530

    STL -> list 0.6: swapping 2 lists content using .swap() function

--- DSA_RED/STL/Containers/lists_.cpp
+++ DSA_RED/STL/Containers/lists_.cpp
@@ -3,14 +3,22 @@
 
 using namespace std;
 
-int main(){
-    list<int> list1 = {1, 20, 3, 4, 5};
-
-    list<int>::iterator it = list1.begin();
-    while (it != list1.end()) {
-        cout << *it << " ";
-        it++;
+void printList(const list<int>& lst) {
+    for (const int& val : lst) {
+        cout << val << " ";
     }
+    cout << endl;
+}
+
+int main(){
+    list<int> list1 = {1, 2, 3, 4, 5};
+    list<int> list2 = {10, 20, 30, 40, 50};
+
+    // Swapping List Contents
+    list1.swap(list2);
+    
+    printList(list1); // Outputs: 10 20 30 40 50 
+    printList(list2); // Outputs: 1 2 3 4 5 
 
     return 0;
 }
