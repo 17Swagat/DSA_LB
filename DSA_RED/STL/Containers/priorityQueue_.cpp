9a12ad612531972aa207be0ff701f015fb682edc
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 20:02:46 +0530

    STL -> PriorityQueue 0.2: Iterating through a priority queue is not allowed. Got keeping using .top() & .pop() to pop out elements

--- DSA_RED/STL/Containers/priorityQueue_.cpp
+++ DSA_RED/STL/Containers/priorityQueue_.cpp
@@ -6,14 +6,18 @@ using namespace std;
 
 int main(){
     priority_queue<int> pq; // max heap :=> "Largest element at the top (i.e Gets Highest Priority)"
+
     pq.push(10);
-    pq.push(5);
     pq.push(20);
-    cout<<pq.top()<<endl; // 20
-    pq.pop();
-    cout<<pq.top()<<endl; // 10
-    pq.pop();
-    cout<<pq.top()<<endl; // 5
+    pq.push(30);
+    pq.push(22);
+
+    //*  iterating through the priority queue is not possible
+    while (!pq.empty()) {
+        cout << pq.top() << " "; // 30 22 20 10
+        pq.pop();
+    }
+
 
     return 0;
 }
