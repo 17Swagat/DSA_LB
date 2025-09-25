04be16a0a83d389ca5cd5175788fad2b8ce96793
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 19:44:24 +0530

    STL -> Deque 0.2: .pop_back(), .pop_front() in Deque

--- DSA_RED/STL/Containers/dequeu_.cpp
+++ DSA_RED/STL/Containers/dequeu_.cpp
@@ -3,34 +3,25 @@
 
 using namespace std;
 
-/*
- Deque: Double Ended Queue
-    Allows insertion and deletion from both ends
-    Implemented as a dynamic array of fixed-size arrays
-    Provides random access to elements
-    More memory overhead compared to vector
-    Suitable for scenarios requiring frequent insertions and deletions at both ends
-    
-    Time Complexity:
-        Insertion/Deletion at both ends: O(1)
-        Random Access: O(1)
- */
+void printDeque(deque<int> &d){
+    for(auto i : d){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
 
 int main(){
     // #1
     deque<int> d = {1, 2, 3};
-    for (int i = 0; i < d.size(); i++){
-        cout << d[i] << " "; // Output: 1 2 3
-    }
+    printDeque(d);
+    
+    // Pop Back
+    d.pop_back();
+    printDeque(d);
 
-    cout << "\n";
-
-    // #2
-    d.push_back(4); // Insert at the back
-    d.push_front(0); // Insert at the front
-    for (int i = 0; i < d.size(); i++){
-        cout << d.at(i) << " "; // Output: 0 1 2 3 4
-    }
+    // Pop Front
+    d.pop_front();
+    printDeque(d);
 
     return 0;
 }
