cf20e315765143f39a2a06d73e3954eba52c99f0
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 18:51:06 +0530

    STL -> Queue 0.1: Declaring/Initializing, Pushing elements and tranversing through the elements in a Queue

--- DSA_RED/STL/Containers/queue_.cpp
+++ DSA_RED/STL/Containers/queue_.cpp
@@ -3,18 +3,27 @@
 
 using namespace std;
 
-/*
- STL Queue
-    - FIFO (First In First Out)
-    - push() -> O(1)
-    - pop() -> O(1)
-    - front() -> O(1)
-    - back() -> O(1)
-    - size() -> O(1)
-    - empty() -> O(1)
- */
 
 int main(){
     
+    // What's wrong?
+    // queue<int> q = {1, 2, 3, 4, 5}; // ❌ [Error:]
+    // [Correct way]:=>
+    queue<int> q;
+    // Pushing elements into the queue
+    for (int i = 1; i <= 5; i++){
+        q.push(i);
+    }
+
+    // Last Element in Queue
+    cout << "Last Element: " << q.back() << endl;   // 5
+
+    // Unlike vectors/lists, in order to tranverse a queue, we have to pop elements
+    while (!q.empty()){
+        cout << q.front() << " ";
+        q.pop();    
+    }
+
+
     return 0;
 }
