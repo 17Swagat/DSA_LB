87f8be121a986d91b306985e990111f8a6eb6515
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 16:54:47 +0530

    STL -> vectors 0.10: .erase()

--- DSA_RED/STL/Containers/vectors_.cpp
+++ DSA_RED/STL/Containers/vectors_.cpp
@@ -14,14 +14,8 @@ int main(){
     
     vector<int> vec1 = {1, 2, 3, 4, 5};
 
-    // #1
-    vec1.insert(vec1.begin() + 2, 10); // {1, 2, 10, 3, 4, 5}
-    printVector(vec1);
-    
-    // #2
-    vec1 = {1, 2, 3, 4, 5};
-    vec1.insert(vec1.end()-1, 20); // {1, 2, 3, 4, 20, 5}
-    printVector(vec1);
+    vec1.erase(vec1.begin() + 1); // Erase element at index 1
+    printVector(vec1); // Output: 1 3 4 5 
 
     return 0;
 }
