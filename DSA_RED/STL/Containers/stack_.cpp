5d0ccbaf2bafe077467c1a4ca1c6eaccbaf26b19
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 19:04:15 +0530

    STL -> Stack - 0.0

---
+++ DSA_RED/STL/Containers/stack_.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <stack>
+
+/*
+ STL - stack
+    LIFO - Last In First Out
+    push() - add element to the top
+    pop() - remove element from the top
+ */
+
+using namespace std;
+
+int main(){
+    return 0;
+}
