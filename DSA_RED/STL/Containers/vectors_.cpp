fdad9090159872a8ab66b1135e85890ffdb7ec37
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 15:23:56 +0530

    STL -> vectors 0.3: Redeclaring the vector with more elements than the previous size won't cause trouble

--- DSA_RED/STL/Containers/vectors_.cpp
+++ DSA_RED/STL/Containers/vectors_.cpp
@@ -13,10 +13,18 @@ void printVector(vector<int> &vec){
 int main(){
 
     vector<int> vec1(5);
-    vec1 = {1, 2, 3, 4, 5};
+    vec1 = {1, 2, 3, 4, 5, 76};
     for (auto i = vec1.begin(); i != vec1.end(); i++){
         cout << *i << " ";
     }
+    cout << "\n";
+
+    cout << vec1.size() << "\n"; // 6
+    cout << vec1.capacity() << "\n"; // 6
+
+    // difference between size and capacity
+    // size is the number of elements present in the vector
+    // capacity is the number of elements that can be stored in the vector without reallocating memory
 
     return 0;
 }
