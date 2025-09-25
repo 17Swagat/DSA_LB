220d507f9752ff5888bf41e2ae7e4be89ff43335
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 18:15:45 +0530

    STL -> Queue 0.0: Starting

---
+++ DSA_RED/STL/Containers/queue_.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+/*
+ STL Queue
+    - FIFO (First In First Out)
+    - push() -> O(1)
+    - pop() -> O(1)
+    - front() -> O(1)
+    - back() -> O(1)
+    - size() -> O(1)
+    - empty() -> O(1)
+ */
+
+int main(){
+    
+    return 0;
+}
