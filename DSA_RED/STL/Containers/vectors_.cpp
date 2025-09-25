15578d7eb65b89868e1920f9e1ba108d93bd7ea3
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 14:57:39 +0530

    STL -> vectors 0.1

--- DSA_RED/STL/Containers/vectors_.cpp
+++ DSA_RED/STL/Containers/vectors_.cpp
@@ -3,9 +3,30 @@
 
 using namespace std;
 
+void printVector(vector<int> &vec){
+    for (auto it : vec){
+        cout << it << " ";
+    }
+    cout << "\n";
+}
+
 int main(){
 
+    // #1
     vector<int> v;
+    printVector(v);
+
+    // #2
+    vector<int> a(5, 1); // 5 elements with value 1
+    printVector(a);
+
+    // #3
+    vector<int> b(a); // copy of vector a
+    printVector(b);
+
+    // #4
+    vector<int> c(5); // 5 elements with default value 0
+    printVector(c);
 
     return 0;
 }
