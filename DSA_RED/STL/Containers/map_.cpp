9afa61ca9bd8ca4bb412d1d434d65323e05c2747
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 20:17:52 +0530

    STL -> Map 0.2: Map with key, value => <int, pair<string, int>>

--- DSA_RED/STL/Containers/map_.cpp
+++ DSA_RED/STL/Containers/map_.cpp
@@ -5,15 +5,16 @@ using namespace std;
 
 int main()
 {
-    map<int, string> m = {
-        {1, "one"},
-        {2, "two"},
-        {3, "three"}
+    map<int, pair<string, int>> m = {
+        {1, {"student 1", 23}},
+        {2, {"student 2", 53}},
+        {3, {"student 3", 93}}
     };
 
-    for (auto &pr : m) {
-        cout << pr.first << " -> " << pr.second << endl;
+    for (auto &item: m){
+        cout << item.first << " -> " << item.second.first << ", " << item.second.second << endl;
     }
+
 
     return 0;
 }
