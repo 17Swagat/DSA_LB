14060eb11c8647febc932d5b9a288b8f37426afe
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 17:47:15 +0530

    STL -> list 0.5: list iterator and looping through the list

--- DSA_RED/STL/Containers/lists_.cpp
+++ DSA_RED/STL/Containers/lists_.cpp
@@ -4,13 +4,12 @@
 using namespace std;
 
 int main(){
-    list<int> list1 = {1, 2, 3, 4, 5};
+    list<int> list1 = {1, 20, 3, 4, 5};
 
-    list1.pop_back();
-    list1.pop_front();
-
-    for(int x : list1){
-        cout << x << " ";
+    list<int>::iterator it = list1.begin();
+    while (it != list1.end()) {
+        cout << *it << " ";
+        it++;
     }
 
     return 0;
