2c329dbb906cf4b735c35c1590ac8ab6ff82b915
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 15:19:48 +0530

    STL -> vectors 0.2 : Iterating through a vector using vec1.begin() & vec1.end()

--- DSA_RED/STL/Containers/vectors_.cpp
+++ DSA_RED/STL/Containers/vectors_.cpp
@@ -12,21 +12,11 @@ void printVector(vector<int> &vec){
 
 int main(){
 
-    // #1
-    vector<int> v;
-    printVector(v);
-
-    // #2
-    vector<int> a(5, 1); // 5 elements with value 1
-    printVector(a);
-
-    // #3
-    vector<int> b(a); // copy of vector a
-    printVector(b);
-
-    // #4
-    vector<int> c(5); // 5 elements with default value 0
-    printVector(c);
+    vector<int> vec1(5);
+    vec1 = {1, 2, 3, 4, 5};
+    for (auto i = vec1.begin(); i != vec1.end(); i++){
+        cout << *i << " ";
+    }
 
     return 0;
 }
