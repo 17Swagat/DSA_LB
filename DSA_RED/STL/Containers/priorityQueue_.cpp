6cfe929fd7f9ccc030a83ce17594bd551c4b7446
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 20:10:03 +0530

    STL -> PriorityQueue 0.4: MinHeap example

--- DSA_RED/STL/Containers/priorityQueue_.cpp
+++ DSA_RED/STL/Containers/priorityQueue_.cpp
@@ -8,5 +8,15 @@ int main()
     // min-heap
     priority_queue<int,vector<int>,greater<int>>pq; // min-heap
 
+    pq.push(10);
+    pq.push(5);
+    pq.push(20);    
+    pq.push(15);
+
+    while (!pq.empty()) {
+        cout << pq.top() << " "; // 5 10 15 20
+        pq.pop();
+    }
+
     return 0;
 }
