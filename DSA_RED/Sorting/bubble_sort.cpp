01c144db8ad1ab867edd3908e8a02acf6dd969ee
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-03 15:23:51 +0530

    Bubble Sort 0.2 -> In Decreasing Order

--- DSA_RED/Sorting/bubble_sort.cpp
+++ DSA_RED/Sorting/bubble_sort.cpp
@@ -12,7 +12,7 @@ void bubbleSort(vector<int> &nums){
 
     for (int i = 0; i < n-1; i++){
         for (int k = 0; k < n-1-i; k++) {
-            if (nums[k] > nums[k+1]) {
+            if (nums[k] < nums[k+1]) {
                 int temp = nums[k];
                 nums[k] = nums[k+1];
                 nums[k+1] = temp;
@@ -25,7 +25,7 @@ void bubbleSort(vector<int> &nums){
 
 int main(){
 
-    vector<int> nums = {5, 4, 3, 2, 1};
+    vector<int> nums = {5, 0, 4, -1, 33, 64, 2};
 
     bubbleSort(nums);
 
