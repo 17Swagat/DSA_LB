aafe5d60a07d91d34d84fa10ef5c010a790c0d1b
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-04 13:55:47 +0530

    c++ sorting a 2D vector based on its 2nd element

--- DSA_RED/Sorting/cpp_sortFunc.cpp
+++ DSA_RED/Sorting/cpp_sortFunc.cpp
@@ -4,35 +4,24 @@
 
 using namespace std;
 
-bool my_comparator(int &a, int &b) {
-    return (a>b);
-}
 
 int main(){
 
-    // #1
-    // int arr1[] = {5, 23, 1, 0, 10};
-    // sort(arr1, arr1 + sizeof(arr1)/sizeof(int));
-    // for (auto i: arr1) {
-    //     cout << i << " ";
-    // }
+    vector<vector<int>> vec = {
+        {1, 44}, {0, 55}, {0, 22}, {0, 11}, {2, 33}
+    };
 
-    // #2
-    // vector<int> vec1 = {5, 23, 1, 0, 10};
-    // sort(vec1.begin(), vec1.end());  // sorting in <ASCENDING ORDER>
-    // for (auto i: vec1) {
-    //     cout << i << " ";
-    // }
+    // Tasks: Sort the Above 2D vector in DESCENDING ORDER, based on their "2nd element"
 
-    // #3
-    vector<int> vec1 = {5, 23, 1, 0, 10};
-    // sort(vec1.begin(), vec1.end(), my_comparator);  // sorting in <DESCENDING ORDER>
-    // sorting in <DESCENDING ORDER>
-    sort(vec1.begin(), vec1.end(), [](int &a, int &b){
-        return (a > b);
-    }); 
-    for (auto i: vec1) {
-        cout << i << " ";
+    sort(vec.begin(), vec.end(), [](vector<int> &a, vector<int> &b){
+        return (a[1] > b[1]);
+    });
+
+    for (auto v: vec) {
+        for (int i: v) {
+            cout << i << " ";
+        }
+        cout << " ";
     }
     
     return 0;
