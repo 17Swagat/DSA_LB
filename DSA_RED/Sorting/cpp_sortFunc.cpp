58b2d89e69615379764804239f12ed5beeb3e1f3
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-04 13:59:55 +0530

    c++ sorting a 2D vector based on its 2nd element

--- DSA_RED/Sorting/cpp_sortFunc.cpp
+++ DSA_RED/Sorting/cpp_sortFunc.cpp
@@ -12,9 +12,8 @@ int main(){
     };
 
     // Tasks: Sort the Above 2D vector in DESCENDING ORDER, based on their "2nd element"
-
     sort(vec.begin(), vec.end(), [](vector<int> &a, vector<int> &b){
-        return (a[1] > b[1]);
+        return (a[1] < b[1]);
     });
 
     for (auto v: vec) {
