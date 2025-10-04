8ee89686549948bae3973ce21936a191b0c79d1e
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-04 16:21:50 +0530

    Binary Search Algo -> First Occurrence of target.

---
+++ DSA_RED/Searching/binary_search_1stOccurence.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int firstOccurence(int target, vector<int> &arr){
+    // Assuming: `arr` -> Sorted in "Increasing Order"
+    int s = 0;
+    int e = arr.size() - 1;
+    int store = -1;
+    while (s <= e) {
+        int mid = (s + (e-s)/2);
+
+        if (target == arr[mid]) {
+            store = mid;
+            e = mid - 1;
+            continue;
+        } else if (target > arr[mid]) {
+            s = mid + 1;
+        } else {
+            // target < arr[mid]
+            e = mid - 1;
+        }
+    }
+
+    return store;
+}
+
+int main(){
+    vector<int> nums = {10, 11, 20, 20, 20, 30, 40, 40, 50};
+    int ans = firstOccurence(40, nums);
+    cout << ans << "\n";
+    
+    return 0;
+}
