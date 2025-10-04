a6634a8c9056ee9f3767f5c58e6c6bb6b933f63d
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-04 13:14:56 +0530

    Selection Sort 0.1

---
+++ DSA_RED/Sorting/selection_sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selectionSort(vector<int> &nums){
+    int n = nums.size();
+    if (n == 1) {
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int smallest = nums[i];
+        int smallest_index = i;
+        for (int k = i+1; k < n; k++){
+            if (smallest > nums[k]) {
+                smallest = nums[k];
+                smallest_index = k;
+            }
+        }
+        // Swap:
+        {
+            int temp = nums[i];
+            nums[i] = nums[smallest_index];
+            nums[smallest_index] = temp;
+        }
+    }
+}
+
+int main(){
+
+    vector<int> nums = {-3,10, 5, 4, 3}; // {3, 4, 5, 10}
+
+    selectionSort(nums);
+
+    for (int i: nums) {
+        cout << i << " ";
+    }
+    
+    return 0;
+}
