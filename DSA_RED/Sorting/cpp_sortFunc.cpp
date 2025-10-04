deb4c5a1f75ac1c050e21704b3100c16fd10e8b6
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-04 13:43:30 +0530

    c++ sorting vector in DESCENDING ORDER

--- DSA_RED/Sorting/cpp_sortFunc.cpp
+++ DSA_RED/Sorting/cpp_sortFunc.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
+bool my_comparator(int &a, int &b) {
+    return (a>b);
+}
 
 int main(){
 
-    int arr1[] = {5, 23, 1, 0, 10};
-    sort(arr1, arr1 + sizeof(arr1)/sizeof(int));
-    for (auto i: arr1) {
+    // #1
+    // int arr1[] = {5, 23, 1, 0, 10};
+    // sort(arr1, arr1 + sizeof(arr1)/sizeof(int));
+    // for (auto i: arr1) {
+    //     cout << i << " ";
+    // }
+
+    // #2
+    // vector<int> vec1 = {5, 23, 1, 0, 10};
+    // sort(vec1.begin(), vec1.end());  // sorting in <ASCENDING ORDER>
+    // for (auto i: vec1) {
+    //     cout << i << " ";
+    // }
+
+    // #3
+    vector<int> vec1 = {5, 23, 1, 0, 10};
+    sort(vec1.begin(), vec1.end(), my_comparator);  // sorting in <DESCENDING ORDER>
+    for (auto i: vec1) {
         cout << i << " ";
     }
     
